2ae2df59cd05de910369befced0f8aea348c8a06
AdrianRamadhan27 <ramadhanadrian2710@gmail.com>
2022-09-09 10:01:27 +0700

    Added Model Testing

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
-
+from katalog.models import CatalogItem
 # Create your tests here.
+
+class TestModels(TestCase):
+    def setUp(self):
+        self.catalog_item1 = CatalogItem.objects.create(
+            item_name= "Nike Air Jordan FISIP",
+            item_price = 3789000,
+            description = "Nike Original Made In Taiwan",
+            item_stock = 30,
+            rating = 4,
+            item_url = "https://www.tokopedia.com/807garage/air-jordan-1-mid-multicolour"
+        )
+
+    def test_catalog_item(self):
+        self.assertTrue(isinstance(self.catalog_item1, CatalogItem))
+   
