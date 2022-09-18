10e00e65b3e4132dfd5b16df2a445514c1010762
AdrianRamadhan27 <ramadhanadrian2710@gmail.com>
2022-09-18 12:26:36 +0700

    Added url for mywatchlist/

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from mywatchlist.views import show_html, show_xml, show_json
 urlpatterns = [
+    path('', show_html, name='watchlist_home'),
     path('html/', show_html, name='show_html'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
