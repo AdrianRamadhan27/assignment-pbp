cb85e5289571cf99228796d2d5ab664932d09fc7
AdrianRamadhan27 <ramadhanadrian2710@gmail.com>
2022-09-18 07:51:01 +0700

    Added unit testing

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
 # Create your tests here.
+class MyWatchlistViewsTest(TestCase):
+    client = Client()
+    def test_html(self):
+        response = self.client.get(reverse('show_html'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'mywatchlist.html')
+
+    def test_xml(self):
+        response = self.client.get(reverse('show_xml'))
+        self.assertEquals(response.status_code, 200)
+        
+
+    def test_json(self):
+        response = self.client.get(reverse('show_json'))
+        self.assertEquals(response.status_code, 200)
+        
